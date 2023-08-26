e0a12418b5ea9c85354f240a65e7171958a68f76
kujawapatryk <homik363@wp.pl>
2023-08-26 19:47:22 +0200

    add Snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SnackbarProvider } from 'notistack';
+import { Tasks } from './componensts/Tasks/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SnackbarProvider maxSnack={5}>
+        <Tasks />
+      </SnackbarProvider>
+    </>
   );
 }
 
