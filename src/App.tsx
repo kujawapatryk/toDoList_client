e56a281e3601fa332313d99c83d67c0ccf05454d
kujawapatryk <homik363@wp.pl>
2023-08-27 02:00:11 +0200

    Adding Checkbox, Styling Change

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { SnackbarProvider } from 'notistack';
 import { Tasks } from './componensts/Tasks/Tasks';
+import { TaskView } from './views/TaskView/TaskView';
+
 
 function App() {
   return (
     <>
       <SnackbarProvider maxSnack={5}>
-        <Tasks />
+          <TaskView />
       </SnackbarProvider>
     </>
   );
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,6 +1,19 @@
 
+type Messages = {
+    [key: string]: {
+        value: string;
+        variant: 'default' | 'error' | 'success' | 'warning' | 'info' | undefined;
+    };
+}
 
-export const message ={
-    done: {value: "Zrobione"},
-    deleteTask: {value: "Usuń"},
+export const message: Messages ={
+    done: {value: "Wykonane", variant: undefined},
+    deleteTask: {value: "Usuń", variant: undefined},
+    taskConfirmation: { value: 'Zadanie oznaczone jako wykonane.', variant: 'success' },
+    taskDeleted: { value: 'Zadanie usunięte.', variant: 'success' },
+    error: { value: 'Nieznany błąd.', variant: 'error' },
+    taskAdded: { value: 'Zadanie dodane.', variant: 'success' },
+    addTaskError: { value: 'Nie udało się dodać zadania. Spróbuj ponownie.', variant: 'error' },
+    maxCharacterCount : {value: 'Maksymalna ilość znaków to 120', variant: 'warning' },
+    emptyFieldError: { value: 'Pole zadanie nie może być puste.', variant: 'error' },
 }
